e82a63284f6190a49ebeeb4a6bb81601f11c5cbc
Gokul <catchgokul@gmail.com>
2021-04-01 16:45:40 -0400

    GNH-7 changing blog titles font size, adding bitbucket-pipelines env variables to use repository variables for gcloud deploy

--- components/Blog.js
+++ components/Blog.js
@@ -21,7 +21,7 @@ class Blog extends React.Component {
                 background: '#FFF',
                 overflow: 'visible',
                 width: '100%',
-                height: '4920px'}
+                height: '1920px'}
               }>
             </iframe>
                 {/*  style='border-width:0px; border-color:#333; background:#FFF; border-style:solid;'> */}
--- components/blog/intro.js
+++ components/blog/intro.js
@@ -3,8 +3,8 @@ import { CMS_NAME, CMS_URL } from '../../lib/constants'
 export default function Intro() {
   return (
     <section className="flex-col md:flex-row flex items-center md:justify-between mt-1 mb-2 md:mb-12">
-      <h4 className="text-center md:text-left text-md mt-1 md:pl-8">
-        A statically generated blog example using{' '}
+      <h5 className="text-center md:text-left text-md mt-1 md:pl-8">
+        A statically generated blog using{' '}
         <a
           href="https://nextjs.org/"
           className="underline hover:text-success duration-200 transition-colors"
@@ -19,7 +19,7 @@ export default function Intro() {
           Sanity.io
         </a>
         .
-      </h4>
+      </h5>
     </section>
   )
 }
