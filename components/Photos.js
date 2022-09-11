c7bd3065064c8111cabcdd1a31f31462416799f6
gokulmenon <catchgokul@gmail.com>
2022-09-11 18:21:20 -0400

    GNH-68 disabling fullscreen button on main gallery

--- components/Photos.js
+++ components/Photos.js
@@ -13,9 +13,6 @@ class Photos extends React.Component {
       this.state = {
         showPlayButton: true,
         showGalleryPlayButton: true,
-        showFullscreenButton: true,
-        useBrowserFullscreen: false,
-        showGalleryFullscreenButton: true,
         showVideo: {},
       };
       this.images = [
@@ -79,10 +76,6 @@ class Photos extends React.Component {
     if (this.state.showPlayButton) {
       this.setState({showGalleryPlayButton: true});
     }
-
-    if (this.state.showFullscreenButton) {
-      this.setState({showGalleryFullscreenButton: true});
-    }
   }
 
   render() {
@@ -99,7 +92,7 @@ class Photos extends React.Component {
       <ImageGallery 
         items={this.images} 
         showNav={true}
-        showFullscreenButton={this.state.showFullscreenButton && this.state.showGalleryFullscreenButton}
+        showFullscreenButton={false}
         showPlayButton={this.state.showPlayButton && this.state.showGalleryPlayButton}
       />
       <br />
