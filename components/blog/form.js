71466feee0bdacea6ce5626348161b003b9a7fb7
gokulmenon <catchgokul@gmail.com>
2024-03-31 13:22:47 -0400

    fixing bugs in forms.js due to upgrading rhf v7

--- components/blog/form.js
+++ components/blog/form.js
@@ -5,7 +5,7 @@ export default function Form ({_id}) {
   const [formData, setFormData] = useState()
   const [isSubmitting, setIsSubmitting] = useState(false)
   const [hasSubmitted, setHasSubmitted] = useState(false)
-  const { register, handleSubmit, watch, errors } = useForm()
+  const { register, handleSubmit, setError, formState:{errors}, } = useForm()
   const onSubmit = async data => {
     setIsSubmitting(true)
     let response
@@ -41,21 +41,24 @@ export default function Form ({_id}) {
 
   return (
     <form onSubmit={handleSubmit(onSubmit)} className="w-full max-w-lg" disabled>
-      <input ref={register} type="hidden" name="_id" value={_id} />
+      <input {...register('_id')} type="hidden" name="_id" value={_id} />
       <label className="block mb-5">
         <span className="text-gray-400">Name</span>
-        <input name="name" type="text" ref={register({required: true})} className="shadow border rounded py-2 px-3 form-input mt-1 block w-full" placeholder="First Last "/>
+        <input name="name" type="text" {...register("name",{required: true})} className="shadow border rounded py-2 px-3 form-input mt-1 block w-full" placeholder="First Last "/>
       </label>
+      {errors.name && <span>Field is reqeuired !</span>}
       <label className="block mb-5">
         <span className="text-gray-400">Email</span>
-        <input name="email" type="email" ref={register({required: true})} className="shadow border rounded py-2 px-3 form-input mt-1 block w-full" placeholder="your@email.com"/>
+        <input name="email" type="email" {...register("email",{required: true})} className="shadow border rounded py-2 px-3 form-input mt-1 block w-full" placeholder="your@email.com"/>
       </label>
+      {errors.email && <span>Field is reqeuired !</span>}
       <label className="block mb-5">
         <span className="text-gray-400">Comment</span>
-        <textarea ref={register({required: true})} name="comment" className="shadow border rounded py-2 px-3  form-textarea mt-1 block w-full" rows="8" placeholder="Enter your comment"></textarea>
+        <textarea {...register("comment",{required: true})} name="comment" className="shadow border rounded py-2 px-3  form-textarea mt-1 block w-full" rows="8" placeholder="Enter your comment"></textarea>
       </label>
       {/* errors will return when field validation fails  */}
-      {errors.exampleRequired && <span>This field is required</span>}
+      {errors.comment && <span>Field is reqeuired !</span>}
+      <br/>
       <input type="submit" className="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" />
     </form>
 )
